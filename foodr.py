fb0cf8109744aa5f50a882488094cfbf54bebcf7
ahaliyara <ahaliyara@gmail.com>
2025-07-22 22:13:35 -0700

    Total revenue per meal
    using map is same with join statement

--- foodr.py
+++ foodr.py
@@ -32,3 +32,11 @@ print("\n✅ Task 1: Unique counts")
 print("Unique meals:", meals_df["meal_id"].nunique())
 print("Unique eateries:", meals_df["eatery"].nunique())
 
+# ▶️ Step 3: Total revenue per meal
+orders_df["order_revenue"] = orders_df["order_quantity"] * orders_df["meal_id"].map(
+   meals_df.set_index("meal_id")["meal_price"]
+)
+revenue_per_meal = orders_df.groupby("meal_id")["order_revenue"].sum().reset_index()
+revenue_per_meal = revenue_per_meal.merge(meals_df[["meal_id", "eatery"]], on="meal_id", how="left")
+print("\n✅ Task 2: Top 5 revenue-generating meals:")
+print(revenue_per_meal.sort_values(by="order_revenue", ascending=False).head())
